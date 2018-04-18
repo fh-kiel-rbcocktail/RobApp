7d5adddba89d99839241af237c651da8f00d26f7
Hai Huynh <Nhut.H.Huynh@student.fh-Kiel.de>
2018-04-19 04:55:51 +0700

    Ingredient Examples

---
+++ HelloWorldFlexFellow/src/application/Orange.java
@@ -0,0 +1,10 @@
+package application;
+
+public class Orange extends Ingredient {
+	
+	public Orange(float timeToFill) {
+		super(timeToFill);
+		this.name = "Orange Juice";
+		this.weightPerVolume = 1.05;
+	}
+}
